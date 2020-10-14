250a2607cd19332c3fe871af19bdf7ef1b4370f6
LeviCoelho <coelhobelem@hotmail.com>
2020-10-13 22:31:52 -0300

    Adicionado o reconhecimento facial no Teste.py mas ainda não foi testado no drone

--- Teste.py
+++ Teste.py
@@ -2,8 +2,8 @@ import sys
 import traceback
 import tellopy
 import av
-import cv2.cv2 as cv2  # for avoidance of pylint error
-import cv2 as os  # for avoidance of pylint error
+#import cv2.cv2 as cv2  # for avoidance of pylint error
+import cv2, os  # for avoidance of pylint error
 import numpy
 import time
 
@@ -12,11 +12,11 @@ def find(name, path):
         if (name in files) or (name in dirs):
             return os.path.join(root, name)
     # Caso nao encontre, recursao para diretorios anteriores
-    return find(name, cv2.path.dirname(path))
+    return find(name, os.path.dirname(path))
 
 def main():
     drone = tellopy.Tello()
-
+    
     cv2path = os.path.dirname(cv2.__file__)
     haar_path = find('haarcascades', cv2path)
     xml_name = 'haarcascade_frontalface_alt2.xml'
@@ -25,7 +25,6 @@ def main():
     # TODO: Inicializar Classificador
     clf = os.CascadeClassifier(xml_path)
     
-    global font
     try:
         drone.connect()
         drone.wait_for_connection(60.0)
@@ -50,7 +49,22 @@ def main():
                 start_time = time.time()
                 image = cv2.cvtColor(numpy.array(frame.to_image()), cv2.COLOR_RGB2BGR)
                 cv2.imshow('Original', image)
-                cv2.imshow('Canny', cv2.Canny(image, 100, 200))
+                #cv2.imshow('Canny', cv2.Canny(image, 100, 200))
+                
+                # TODO: Converter para tons de cinza
+                gray = cv2.cvtColor(numpy.array(frame.to_image()), cv2.COLOR_BGR2GRAY)
+            
+                # TODO: Classificar
+                faces = clf.detectMultiScale(gray)
+            
+                # TODO: Desenhar retangulo
+                for x, y, w, h in faces:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0))
+            
+                # Visualizar
+                cv2.imshow('frame',frame)
+                
+                
                 cv2.waitKey(1)
                 if frame.time_base < 1.0/60:
                     time_base = 1.0/60
