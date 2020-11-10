288071d7d1cff8b6d21f4f70967220a97fa229b1
LeviCoelho <coelhobelem@hotmail.com>
2020-11-09 21:52:33 -0300

    Precisa ajustar o tempo para testar

--- multi-thread Python/TCC_Main.py
+++ multi-thread Python/TCC_Main.py
@@ -13,13 +13,7 @@ import pygame.locals
 import pygame.font
 import threading
 
-tempo = time.time()
-     
-
 date_fmt = '%Y-%m-%d_%H%M%S'
-
-def status_print(text):
-    pygame.display.set_caption(text)
 
 def find(name, path):
     for root, dirs, files in os.walk(path):
@@ -28,46 +22,12 @@ def find(name, path):
     # Caso nao encontre, recursao para diretorios anteriores
     return find(name, os.path.dirname(path))
 
-
-class minhaThread (threading.Thread):
-    tempo = time.time()
- 
-    def __init__(self, threadID, nome):
-        #my_drone = drone
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.nome = nome
-
-    def run(self):
-        tempoTotal = time.time() - tempo
-    
-        if 30 >= tempoTotal:
-            print("takeoff")
-            #drone.takeoff()
-        if tempoTotal >= 60 and 30 >= tempoTotal:
-            print("forward")
-            #my_drone.forward(15)
-        if tempoTotal >= 90:
-            print("land")
-            #my_drone.land()
-        print("Tempo Thread %d" % (tempoTotal))
-        # while True:
-        #     time.sleep(1)
-        #     tempoTotal = time.time() - tempo
-        #     print("Tempo Thread %d" % (tempoTotal))
-
-def processo(nome, contador):
-    while contador:
-        print ("Thread %s fazendo o processo %d" % (nome, contador))
-        contador -= 1
-
-
-
 def main():
 
     drone = tellopy.Tello()
+    tempo = time.time()
+
     # Criando as threads
-    thread1 = minhaThread(1, "Mover")
     xml_path = '/home/levi/TCC_Tello/haarcascade_frontalface_alt2.xml' 
     
     # TODO: Inicializar Classificador
@@ -89,24 +49,24 @@ def main():
 
         # skip first 300 frames
         frame_skip = 300
-        setup = True
         while True:
             for frame in container.decode(video=0):
                 if 0 < frame_skip:
                     frame_skip = frame_skip - 1
                     print('Wait %s/300'%frame)
                     continue
-                
-                if setup:
-                    print("Iniciando thread")
-                    time.sleep(10)
-                    # Comecando novas Threads
-                    thread1.start()
-                    
-                    threads = []
-                    threads.append(thread1)
-                    setup = False
-                    print("Thread iniciada")
+
+                tempoTotal = time.time() - tempo
+                if tempoTotal >= 59 and tempoTotal <= 60:
+                    print("takeoff")
+                    drone.takeoff()
+                if tempoTotal >= 90 and tempoTotal <= 91:
+                    print("forward")
+                    drone.forward(15)
+                if tempoTotal >= 120 and tempoTotal <= 121:
+                    print("land")
+                    drone.land()
+                print("Tempo %d" % (tempoTotal))
                 
                 start_time = time.time()
                 image = cv2.cvtColor(numpy.array(frame.to_image()), cv2.COLOR_BGR2GRAY)
@@ -139,10 +99,6 @@ def main():
     finally:
         drone.quit()
         cv2.destroyAllWindows()
-
-    
-    for t in threads:
-        t.join()
     
 if __name__ == '__main__':
     main()
